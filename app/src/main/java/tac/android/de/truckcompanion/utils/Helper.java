f27e13bfc4e481111afae98e98721e60a4f8c889
Jonas Miederer <jonasmied@gmail.com>
2016-05-21 17:33:03 +0200

    added json helper

--- app/src/main/java/tac/android/de/truckcompanion/utils/Helper.java
+++ app/src/main/java/tac/android/de/truckcompanion/utils/Helper.java
@@ -1,5 +1,11 @@
 package tac.android.de.truckcompanion.utils;
 
+import android.content.Context;
+import android.support.annotation.Nullable;
+
+import java.io.IOException;
+import java.io.InputStream;
+
 /**
  * Created by Jonas Miederer.
  * Date: 08.05.16
@@ -10,5 +16,22 @@ package tac.android.de.truckcompanion.utils;
 public class Helper {
     public static int clip(int value, int lower, int upper) {
         return Math.max(lower, Math.min(upper, value));
+    }
+
+    @Nullable
+    public static String getJsonStringFromAssets(Context context, String filename){
+        String json = null;
+        try{
+            InputStream is = context.getAssets().open(filename);
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, "UTF-8");
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+        return json;
     }
 }
