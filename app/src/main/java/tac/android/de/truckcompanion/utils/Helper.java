70930278395204c74f4d8e67aa5b5c7b658bcf74
Jonas Miederer <jonasmied@gmail.com>
2016-07-03 17:43:34 +0200

    removed skobbler again..

--- app/src/main/java/tac/android/de/truckcompanion/utils/Helper.java
+++ app/src/main/java/tac/android/de/truckcompanion/utils/Helper.java
@@ -1,11 +1,10 @@
 package tac.android.de.truckcompanion.utils;
 
 import android.content.Context;
-import android.content.res.AssetManager;
 import android.support.annotation.Nullable;
-import com.google.common.io.ByteStreams;
 
-import java.io.*;
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * Created by Jonas Miederer.
@@ -20,9 +19,9 @@ public class Helper {
     }
 
     @Nullable
-    public static String getJsonStringFromAssets(Context context, String filename){
+    public static String getJsonStringFromAssets(Context context, String filename) {
         String json = null;
-        try{
+        try {
             InputStream is = context.getAssets().open(filename);
             int size = is.available();
             byte[] buffer = new byte[size];
@@ -34,71 +33,5 @@ public class Helper {
             return null;
         }
         return json;
-    }
-
-    /**
-     * Copies files from assets to destination folder
-     * @param assetManager
-     * @param sourceFolder
-     * @param destinationFolder
-     * @throws IOException
-     */
-    public static void copyAssetsToFolder(AssetManager assetManager, String sourceFolder, String destinationFolder)
-            throws IOException {
-        final String[] assets = assetManager.list(sourceFolder);
-
-        final File destFolderFile = new File(destinationFolder);
-        if (!destFolderFile.exists()) {
-            destFolderFile.mkdirs();
-        }
-        copyAsset(assetManager, sourceFolder, destinationFolder, assets);
-    }
-
-    /**
-     * Copies files from assets to destination folder
-     * @param assetManager
-     * @param sourceFolder
-     * @param destinationFolder
-     * @param assetsNames
-     * @throws IOException
-     */
-    public static void copyAsset(AssetManager assetManager, String sourceFolder, String destinationFolder,
-                                 String... assetsNames) throws IOException {
-
-        for (String assetName : assetsNames) {
-            OutputStream destinationStream = new FileOutputStream(new File(destinationFolder + "/" + assetName));
-            String[] files = assetManager.list(sourceFolder + "/" + assetName);
-            if (files == null || files.length == 0) {
-
-                InputStream asset = assetManager.open(sourceFolder + "/" + assetName);
-                try {
-                    ByteStreams.copy(asset, destinationStream);
-                } finally {
-                    asset.close();
-                    destinationStream.close();
-                }
-            }
-        }
-    }
-
-    /**
-     * Copies files from assets to destination folder.
-     * @param assetManager
-     * @param assetName the asset that needs to be copied
-     * @param destinationFolder path to folder where you want to store the asset
-     * archive
-     * @throws IOException
-     */
-    public static void copyAsset(AssetManager assetManager, String assetName, String destinationFolder)
-            throws IOException {
-
-        OutputStream destinationStream = new FileOutputStream(new File(destinationFolder + "/" + assetName));
-        InputStream asset = assetManager.open(assetName);
-        try {
-            ByteStreams.copy(asset, destinationStream);
-        } finally {
-            asset.close();
-            destinationStream.close();
-        }
     }
 }
